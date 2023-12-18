804b0e36d42e6cd4c0841025938411091a6786a1
Giorgi Kintsurashvili <giokintsurashvili0@gmail.com>
2023-12-18 18:47:56 +0400

    test

---
+++ Bookstore/Interfaces/IAuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Bookstore.Models;
+
+namespace Bookstore.Interfaces
+{
+    public interface IAuthorRepository
+    {
+        Task<IEnumerable<Author>> GetAllAuthorsAsync();
+        Task<Author> GetAuthorByIdAsync(int authorId);
+        Task<Author> AddAuthorAsync( Author author);
+        Task<Author> UpdateAuthorAsync(int id, Author author);
+        Task DeleteAuthorAsync(int authorId);
+    }
+}
